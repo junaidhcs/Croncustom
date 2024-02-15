56e19c7c30bcee7176234ac8bb87c9cf6b344b4e
root <root@LAPTOP-J01TTK8G>
2024-02-15 18:00:17 +0530

    first push

---
+++ Cron/Test.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mytask\Croncustom\Cron;
+
+use Psr\Log\LoggerInterface;
+
+class Test {
+    protected $logger;
+    public function __construct(LoggerInterface $logger) {
+        $this->logger = $logger;
+    }
+   /**
+    * Write to system.log
+    *
+    * @return void
+    */
+    public function execute() {
+        $this->logger->info('Cron Works');
+    }
+}
